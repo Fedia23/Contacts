053c424ab00d2bf895fed04b8feefbc08f1da1a3
Fedia23 <fedia.ch@gmail.com>
2021-04-28 13:06:46 +0300

    Hello back-end

--- src/test/java/com/example/contacts/services/ContactsServiceTest.java
+++ src/test/java/com/example/contacts/services/ContactsServiceTest.java
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@SpringBootTest
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = { ContactsApplication.class })
 class ContactsServiceTest {
